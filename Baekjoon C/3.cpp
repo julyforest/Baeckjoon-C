3c6a6bb5cb848a02a43ec08518f640bf433a663b
julyforest <leeharin58@gmail.com>
2022-02-01 21:38:58 +0900

    2022-02-01 #01~02

---
+++ Baekjoon C/3.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+	/*printf("\\    /\\\n");
+	printf(" )  ( ')\n");
+	printf("(  /  )\n");
+	printf(" \\(__)|\n");*/
+	
+	/*printf("|\\_/|\n");
+	printf("|q p|   /}\n");
+	printf("( 0 )\"\"\"\\\n");
+	printf("|\" ^ \"`    |\n");
+	printf("||_/=\\\\__|\n");*/
+
+	int A;
+	int B;
+	scanf("%d %d", &A, &B);
+	printf("%d", A + B);
+
+}
